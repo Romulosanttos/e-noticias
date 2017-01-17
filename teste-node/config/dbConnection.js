6fbd5c5dffbc45e467894f3a28172a29024a356e
Rômulo Santos <romulosantos11@hotmail.com>
2017-01-17 05:23:13 -0300

    utilizando consigh

--- teste-node/config/dbConnection.js
+++ teste-node/config/dbConnection.js
@@ -1,10 +1,15 @@
 var mysql =require("mysql");
 
-module.exports = function(){	
-	return  mysql.createConnection({ //coneção com banco sql
+var connMySQL = function(){
+	 	console.log("conexão esabelecida com database");
+		return  mysql.createConnection({ //conexão com banco sql
 		host : 'localhost',
 		user : 'root',
 		password : 'qwer!123',
 		database : 'portal_noticias'
 	});
+}
+module.exports = function(){	
+	console.log("O autoload carregou o modulo");
+	return connMySQL;
 }
--- teste-node/config/server.js
+++ teste-node/config/server.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var consign = require('consign');
+
 var app =express();
 
 app.set('view engine','ejs');
-app.set('views','./app/views')
+app.set('views','./app/views');
+
+consign()
+	.include('app/routes')
+	.then('config/dbConnection.js')
+	.into(app);// estou scanner em toda a pasta routes para incluir em app
 
 module.exports = app;
